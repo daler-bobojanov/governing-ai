c3bfd08ebec6e8013a1136221c3756213c2b0116
Jiaxi <dev.jiaxiwu@gmail.com>
2020-04-30 15:18:45 -0400

    made Disability, Politic, and Politics components

---
+++ src/components/Politics.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Politics extends Component {
+    state = {  }
+    render() { 
+        return ( <div>
+            
+        </div> );
+    }
+}
+ 
+export default Politics;
