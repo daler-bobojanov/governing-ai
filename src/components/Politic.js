c437fc3ad2d36ea73bb848e46bd6447e2fa5611d
Jiaxi <dev.jiaxiwu@gmail.com>
2020-04-30 16:18:18 -0400

    particles

--- src/components/Politic.js
+++ src/components/Politic.js
@@ -4,7 +4,10 @@ class Politic extends Component {
     state = {  }
     render() { 
         return ( <div>
-            
+            <h1> Politics </h1>
+            <p> Another reason why a keener eye should be kept on technology is because of increased capabilities for malicious actors</p>
+            <p> to undermine democracies more covertly and effetively than ever before. Last election, Russia used disinformation campaigns through </p>
+            <p> automated bots to interefere with the public perception. </p>
         </div> );
     }
 }
